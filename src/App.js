57b8c266056d31583c590725eb24a81873b8c910
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2021-11-03 17:57:35 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
 import HomePage from './components/home-page/HomePage';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <BrowserRouter>
         <Navigation />
         <Route path='/' component={HomePage}/>
+        <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+      <footer>
+          <address>
+              created by Tomasz Adamczyk
+          </address>
+      </footer>
+    );
+};
+
+export default Footer;
