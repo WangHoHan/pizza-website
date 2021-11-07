fff0556945f33e76016837dfade032ee1667b7a1
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2021-11-07 10:22:15 +0100

    layout and styling for loading element

--- src/components/loading-element/LoadingElement.js
+++ src/components/loading-element/LoadingElement.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {translate} from '../../dictionaries/translate';
+import verona from '../../assets/pizzas/verona.webp';
 import './LoadingElement.css';
 
 const LoadingElement = () => {
-    return <h1>hello</h1>
+    return (
+        <section className='section-loading-element'>
+            <article className='article-loading-element'>
+                <picture>
+                    <source srcSet={verona} />
+                    <img className='img-loading-element' src={verona} alt={translate[verona]} />
+                </picture>
+                <h1 className='h1-loading-element'>loading...</h1>
+            </article>
+        </section>
+    );
 };
 
 export default LoadingElement;
