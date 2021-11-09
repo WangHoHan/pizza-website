d3ff8e379f693b2eafea2e8f6726e73164a5b4e3
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2021-11-09 20:18:18 +0100

    add flip card

--- src/components/home-page/home-page-element/HomePageElement.js
+++ src/components/home-page/home-page-element/HomePageElement.js
@@ -4,12 +4,19 @@ import './HomePageElement.css';
 const HomePageElement = ({name, price, picture}) => {
     return (
         <article className='article-home-page-element'>
-            <picture className='shopping-cart'>
-                <source srcSet={picture} />
-                <img className='img-home-page-element' src={picture} alt={name} />
-            </picture>
-            <h1 className='h1-home-page-element'>{name.toLowerCase()}</h1>
-            <h4 className='h4-home-page-element'>${price}</h4>
+            <div className='flip-card'>
+                <div className='flip-card-front'>
+                    <picture className='shopping-cart'>
+                        <source srcSet={picture} />
+                        <img className='img-flip-card-front' src={picture} alt={name} />
+                    </picture>
+                    <h1 className='h1-flip-card-front'>{name.toLowerCase()}</h1>
+                    <h4 className='h4-flip-card-front'>${price}</h4>
+                </div>
+                <div className='flip-card-back'>
+                    <h1 className='h1-flip-card-back'>ingredients</h1>
+                </div>
+            </div>
         </article>
     );
 };
