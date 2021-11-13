2c40c4eb043247e3cd1514561c45baabd43f4e46
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2021-11-13 17:42:57 +0100

    bitcoin

--- src/components/home-page/flip-card/flip-card-front/FlipCardFront.js
+++ src/components/home-page/flip-card/flip-card-front/FlipCardFront.js
@@ -9,7 +9,7 @@ const FlipCardFront = ({name, price, picture}) => {
                 <img className='img-flip-card-front' src={picture} alt={name} />
             </picture>
             <h1 className='h1-flip-card-front'>{name.toLowerCase()}</h1>
-            <h4 className='h4-flip-card-front'>${price}</h4>
+            <h4 className='h4-flip-card-front'>₿{price}</h4>
         </div>
     );
 };
