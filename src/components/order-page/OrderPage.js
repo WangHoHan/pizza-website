285fae4ef51beca09520d4e7a01bd861dce06b91
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2021-11-15 22:53:00 +0100

    loading screen on order post

--- src/components/order-page/OrderPage.js
+++ src/components/order-page/OrderPage.js
@@ -1,5 +1,5 @@
 import {delete_cookie} from 'sfcookies'
-import React from 'react';
+import React, {useState} from 'react';
 import {useHistory} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import {actionCreators} from '../../store';
@@ -9,11 +9,13 @@ import {COOKIE_BAG, URL_ORDER} from '../../definitions';
 import {translate} from '../../dictionaries/translate';
 import {pizzaPictureDictionary} from '../../dictionaries/pizzaPictureDictionary';
 import './OrderPage.css';
+import LoadingElement from "../loading-element/LoadingElement";
 
 const OrderPage = () => {
     const dispatch = useDispatch();
     const {clearBag, removeProductFromBag, addIngredientToProduct, removeIngredientFromProduct, addSauceToBag, removeSauceFromBag} = bindActionCreators(actionCreators, dispatch);
     const history = useHistory();
+    const [isLoading, setIsLoading] = useState(false);
     const bag = useSelector((state) => state.bag);
     const food = useSelector((state) => state.food);
     const ingredients = useSelector((state) => state.ingredients);
@@ -45,6 +47,7 @@ const OrderPage = () => {
     };
 
     const placeOrder = () => {
+        setIsLoading(true);
         axios.request({
             method: 'post',
             url: URL_ORDER,
@@ -52,94 +55,101 @@ const OrderPage = () => {
         }).then(response => {
             clearBag();
             delete_cookie(COOKIE_BAG);
+            setIsLoading(false);
             history.push('/home');
-        }).catch(error => console.log(error));
+        }).catch(error => {
+            console.log(error);
+            setIsLoading(false);
+        });
     };
 
     return (
-        <main className='main-order-page'>
-            <h1 className='h1-order-page'>your bag</h1>
-            <section className='bag'>
-                {bag.pizza.map((product, idx) => {
-                    return (
-                        <article className='product' key={idx}>
-                            <div className='product-info'>
-                                <h2>{translate[food.find(pizza => pizza.id === product.id).name].toLowerCase()}</h2>
-                                <picture>
-                                    <source srcSet={pizzaPictureDictionary[translate[food.find(pizza => pizza.id === product.id).name]]} />
-                                    <img className='img-order-page' src={pizzaPictureDictionary[translate[food.find(pizza => pizza.id === product.id).name]]} alt={translate[food.find(pizza => pizza.id === product.id).name]} />
-                                </picture>
-                                <ul className='basic-ingredients'>
-                                    {ingredients.filter(ingredient => food.find(pizza => pizza.id === product.id).ingredients.includes(ingredient.id))
-                                        .map(filteredIngredient => {
-                                            return <li className='basic-ingredient' key={filteredIngredient.id}>{translate[filteredIngredient.name].toLowerCase()}</li>
+        <>
+            {isLoading ? <LoadingElement /> :
+            <main className='main-order-page'>
+                <h1 className='h1-order-page'>your bag</h1>
+                <section className='bag'>
+                    {bag.pizza.map((product, idx) => {
+                        return (
+                            <article className='product' key={idx}>
+                                <div className='product-info'>
+                                    <h2>{translate[food.find(pizza => pizza.id === product.id).name].toLowerCase()}</h2>
+                                    <picture>
+                                        <source srcSet={pizzaPictureDictionary[translate[food.find(pizza => pizza.id === product.id).name]]} />
+                                        <img className='img-order-page' src={pizzaPictureDictionary[translate[food.find(pizza => pizza.id === product.id).name]]} alt={translate[food.find(pizza => pizza.id === product.id).name]} />
+                                    </picture>
+                                    <ul className='basic-ingredients'>
+                                        {ingredients.filter(ingredient => food.find(pizza => pizza.id === product.id).ingredients.includes(ingredient.id))
+                                            .map(filteredIngredient => {
+                                                return <li className='basic-ingredient' key={filteredIngredient.id}>{translate[filteredIngredient.name].toLowerCase()}</li>
+                                            })}
+                                    </ul>
+                                </div>
+                                <div className='product-modification'>
+                                    <h2 className='h2-product-modification'>personalize your pizza</h2>
+                                    <ul className='pieces'>
+                                        {ingredients.map(ingredient => {
+                                            return (
+                                                <li className='piece' key={ingredient.id}>
+                                                    <button className='add-additional-piece' type='button' value='add additional piece' onClick={() => addAdditionalPiece(idx, ingredient.id, ingredients.find(elem => elem.id === ingredient.id).price)}>
+                                                        add
+                                                    </button>
+                                                    <button className='remove-additional-piece' type='button' value='remove additional piece' onClick={() => removeAdditionalPiece(idx, ingredient.id, ingredients.find(elem => elem.id === ingredient.id).price)}>
+                                                        remove
+                                                    </button>
+                                                    <span className='additional-piece-amount'>
+                                                        {product.ingredients.filter(topping => topping === ingredient.id).length}
+                                                    </span>
+                                                    <span className='additional-piece-info'>
+                                                        {translate[ingredient.name].toLowerCase()}, ₿{ingredient.price}
+                                                    </span>
+                                                </li>);
                                         })}
-                                </ul>
-                            </div>
-                            <div className='product-modification'>
-                                <h2 className='h2-product-modification'>personalize your pizza</h2>
-                                <ul className='pieces'>
-                                    {ingredients.map(ingredient => {
-                                        return (
-                                            <li className='piece' key={ingredient.id}>
-                                                <button className='add-additional-piece' type='button' value='add additional piece' onClick={() => addAdditionalPiece(idx, ingredient.id, ingredients.find(elem => elem.id === ingredient.id).price)}>
-                                                    add
-                                                </button>
-                                                <button className='remove-additional-piece' type='button' value='remove additional piece' onClick={() => removeAdditionalPiece(idx, ingredient.id, ingredients.find(elem => elem.id === ingredient.id).price)}>
-                                                    remove
-                                                </button>
-                                                <span className='additional-piece-amount'>
-                                                    {product.ingredients.filter(topping => topping === ingredient.id).length}
-                                                </span>
-                                                <span className='additional-piece-info'>
-                                                    {translate[ingredient.name].toLowerCase()}, ₿{ingredient.price}
-                                                </span>
-                                            </li>);
-                                    })}
-                                </ul>
-                                <button className='remove-from-bag' type='button' value='remove from bag' onClick={() => removeFromBag(idx)}>
-                                    remove from bag
-                                </button>
-                            </div>
-                        </article>
-                    );
-                })}
-                <article className='sauces'>
-                    <h2 className='h2-sauces'>dressings and sauces</h2>
-                    <ul className='ul-sauces'>
-                        {sauces.map(sauce => {
-                            return (
-                                <li className='sauce' key={sauce.id}>
-                                    <button className='add-sauce' type='button' value='add sauce' onClick={() => addSauce(sauce.id, sauce.price)}>
-                                        add
+                                    </ul>
+                                    <button className='remove-from-bag' type='button' value='remove from bag' onClick={() => removeFromBag(idx)}>
+                                        remove from bag
                                     </button>
-                                    <button className='remove-sauce' type='button' value='remove sauce' onClick={() => removeSauce(sauce.id, sauce.price)}>
-                                        remove
-                                    </button>
-                                    <span className='sauce-amount'>
-                                        {bag.sauce.find(dressing => dressing.id === sauce.id).count}
-                                    </span>
-                                    <span className='sauce-info'>
-                                        {translate[sauce.name].toLowerCase()}, ₿{sauce.price}
-                                    </span>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </article>
-            </section>
-            <section className='order-info'>
-                <article>
-                    <h3 className='h3-order-page'>charge</h3>
-                    <span className='charge'>
-                        ₿{bag.total}
-                    </span>
-                    <button className='place-order' type='submit' value='place order' onClick={() => placeOrder()}>
-                        place order
-                    </button>
-                </article>
-            </section>
-        </main>
+                                </div>
+                            </article>
+                        );
+                    })}
+                    <article className='sauces'>
+                        <h2 className='h2-sauces'>dressings and sauces</h2>
+                        <ul className='ul-sauces'>
+                            {sauces.map(sauce => {
+                                return (
+                                    <li className='sauce' key={sauce.id}>
+                                        <button className='add-sauce' type='button' value='add sauce' onClick={() => addSauce(sauce.id, sauce.price)}>
+                                            add
+                                        </button>
+                                        <button className='remove-sauce' type='button' value='remove sauce' onClick={() => removeSauce(sauce.id, sauce.price)}>
+                                            remove
+                                        </button>
+                                        <span className='sauce-amount'>
+                                            {bag.sauce.find(dressing => dressing.id === sauce.id).count}
+                                        </span>
+                                        <span className='sauce-info'>
+                                            {translate[sauce.name].toLowerCase()}, ₿{sauce.price}
+                                        </span>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </article>
+                </section>
+                <section className='order-info'>
+                    <article>
+                        <h3 className='h3-order-page'>charge</h3>
+                        <span className='charge'>
+                            ₿{bag.total}
+                        </span>
+                        <button className='place-order' type='submit' value='place order' onClick={() => placeOrder()}>
+                            place order
+                        </button>
+                    </article>
+                </section>
+            </main>}
+        </>
     );
 };
 
